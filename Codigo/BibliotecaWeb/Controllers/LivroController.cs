57aed2f15abdff8371736725f1e0472729d261e4
marcosdosea@gmail.com <marcosdosea@gmail.com@444cb355-f9ad-c420-328c-7815d195fa0b>
2013-09-13 02:39:14 +0000

    Relatório de livros por Editora

--- Codigo/BibliotecaWeb/Controllers/LivroController.cs
+++ Codigo/BibliotecaWeb/Controllers/LivroController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Services;
 using Models;
+using Microsoft.Reporting.WebForms;
 
 namespace BibliotecaWeb.Controllers
 {
@@ -117,6 +118,48 @@ namespace BibliotecaWeb.Controllers
         {
             gLivro.Remover(isbn);
             return RedirectToAction("Index");
+        }
+
+        public ActionResult RelatorioLivroEditora()
+        {
+            LocalReport relatorio = new LocalReport();
+
+            //Caminho onde o arquivo do Report Viewer está localizado
+            relatorio.ReportPath = Server.MapPath("~/Reports/ReportLivrosPorEditora.rdlc");
+            //Define o nome do nosso DataSource e qual rotina irá preenche-lo, no caso, nosso método criado anteriormente
+            relatorio.DataSources.Add(new ReportDataSource("DataSetLivro", gLivro.ObterTodos()));
+
+            string reportType = "PDF";
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+            string deviceInfo =
+             "<DeviceInfo>" +
+             " <OutputFormat>PDF</OutputFormat>" +
+             " <PageWidth>9in</PageWidth>" +
+             " <PageHeight>11in</PageHeight>" +
+             " <MarginTop>0.7in</MarginTop>" +
+             " <MarginLeft>2in</MarginLeft>" +
+             " <MarginRight>2in</MarginRight>" +
+             " <MarginBottom>0.7in</MarginBottom>" +
+             "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] bytes;
+
+            //Renderiza o relatório em bytes
+            bytes = relatorio.Render(
+            reportType,
+            deviceInfo,
+            out mimeType,
+            out encoding,
+            out fileNameExtension,
+            out streams,
+            out warnings);
+
+            return File(bytes, mimeType);
         }
     }
 }
